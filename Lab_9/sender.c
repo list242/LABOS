e8f2bcfa0522cdb1984841e12b5abcf62314a426
Funipero <matvej.v@list.ru>
2024-12-20 15:37:08 +0300

    боже убей меня6

--- Lab_9/sender.c
+++ Lab_9/sender.c
@@ -14,10 +14,15 @@ struct sembuf sem_lock = {0, -1, 0};
 struct sembuf sem_open = {0, 1, 0};
 
 const char *shm_name = "my_shared_memory";
-int shmid;
-int semid;
+int shmid = -1;
+int semid = -1;
+char *mes = NULL;
 
 void cleanup(int signum) {
+    if (mes != NULL && mes != (char*)-1) {
+        shmdt(mes);
+    }
+
     if (shmid != -1) {
         shmctl(shmid, IPC_RMID, NULL);
     }
@@ -32,15 +37,20 @@ int main() {
     signal(SIGINT, cleanup);
     signal(SIGTERM, cleanup);
 
-    int fd = open(shm_name, O_CREAT |  O_EXCL | 0666);
+    int fd = open(shm_name, O_CREAT | O_EXCL, 0666);
+    if (fd < 0) {
+        perror("Ошибка при создании файла для ftok");
+        return 1;
+    }
+    close(fd);
+
     key_t key = ftok(shm_name, 11);
     if (key < 0) {
         perror("Ошибка при генерации ключа");
         return 1;
     }
-    close(fd);
 
-    int shmid = shmget(key, 256, 0666 | IPC_CREAT);
+    shmid = shmget(key, 256, 0666 | IPC_CREAT);
     if (shmid < 0) {
         perror("Ошибка при создании сегмента");
         return 1;
@@ -54,10 +64,10 @@ int main() {
 
     if (shmid_ds_info.shm_nattch >= 1) {
         perror("Уже существует");
-        exit(1); 
+        exit(1);
     }
 
-    int semid = semget(key, 1, IPC_CREAT | 0666);
+    semid = semget(key, 1, IPC_CREAT | 0666);
     if (semid < 0) {
         perror("Ошибка при создании семафора");
         return 1;
@@ -65,7 +75,7 @@ int main() {
 
     semctl(semid, 0, SETVAL, 1);
 
-    char *mes = (char*) shmat(shmid, NULL, 0);
+    mes = (char*) shmat(shmid, NULL, 0);
     if (mes == (char*)-1) {
         perror("Ошибка при подключении к сегменту");
         return 1;
@@ -73,16 +83,17 @@ int main() {
 
     while (1) {
         semop(semid, &sem_lock, 1);
-	
+
         time_t now = time(NULL);
         struct tm *tm_info = localtime(&now);
-        
+
         strftime(mes, 256, "%Y-%m-%d %H:%M:%S", tm_info);
         sprintf(mes + strlen(mes), " PID: %d", getpid());
         semop(semid, &sem_open, 1);
-	    sleep(3);
+        sleep(3);
     }
 
+    // Данный код при нормальном завершении тоже выполнит отсоединение
     shmdt(mes);
     shmctl(shmid, IPC_RMID, NULL);
     semctl(semid, 0, IPC_RMID);
